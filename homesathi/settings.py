8bd27953cefeebe07151cf28584dcc8af26b5129
kashish0777 <kashishmotobook@gmail.com>
2026-04-11 17:38:05 +0530

    abc4

--- homesathi/settings.py
+++ homesathi/settings.py
@@ -101,8 +101,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'anisharawat7877@gmail.com'
-EMAIL_HOST_PASSWORD = 'pcdmfssvmniuuamg'
+EMAIL_HOST_USER = 'homesathi.support@gmail.com'
+EMAIL_HOST_PASSWORD = 'kuambfktfczhdrjq'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SITE_NAME = 'RentalHome'
